c0bcfa5629d825e0d8f659f0b36691a37ab80f88
Giovanni Rella <giomrella@gmail.com>
2020-11-04 17:26:03 -0500

    gathering users from github api

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import './App.css';
 import React, { Fragment, Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 
 class App extends Component {
+	state = {
+		users:[],
+		loading: true 
+	};
+	async componentDidMount(){
+		this.setState({ loading: true });
+		const res = await axios.get('https://api.github.com/users');
+		console.log(res.data);
+		this.setState({ users: res.data, loading: false });
+	}
   render() {
     return (
       <Fragment>
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </Fragment>
     );
