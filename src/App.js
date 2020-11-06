13031fc2b9264bb6d43bca8d333687851ebb217a
Giovanni Rella <giomrella@gmail.com>
2020-11-06 11:46:01 -0500

    fully functional app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,82 @@
 import './App.css';
 import React, { Fragment, Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Alert from './components/layout/Alert';
 import Users from './components/users/Users';
+import User from './components/users/User';
+import Search from './components/users/Search';
+import About from './components/pages/About';
 import axios from 'axios';
 
 class App extends Component {
-	state = {
-		users:[],
-		loading: true 
-	};
-	async componentDidMount(){
-		this.setState({ loading: true });
-		const res = await axios.get('https://api.github.com/users');
-		console.log(res.data);
-		this.setState({ users: res.data, loading: false });
-	}
+  state = {
+    users: [],
+    user: {},
+    loading: false,
+    alert: null,
+    repos: []
+  };
+
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  //search github users
+  searchUsers = async text => {
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ users: res.data.items, loading: false });
+  }
+
+  // get single github user
+  getUser = async username => {
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ user: res.data, loading: false });
+  }
+
+  // get users repos 
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ repos: res.data, loading: false });
+  }
+
+  //clear github users
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  //sets an alert 
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  }
+
   render() {
+    const { user, users, repos, loading } = this.state;
     return (
-      <Fragment>
-        <Navbar />
-        <div className="container">
-          <Users loading={this.state.loading} users={this.state.users}/>
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route exact path='/' render={props => (
+                <Fragment>
+                  <Search setAlert={this.setAlert} searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={!!users.length} />
+                  <Users loading={loading} users={users} />
+                </Fragment>
+              )} />
+              <Route exact path='/about' component={About} />
+              <Route exact path='/user/:login' render={props => (
+                <User {...props} getUser={this.getUser} getUserRepos={this.getUserRepos} repos={repos} user={user} loading={loading} />
+              )} />
+            </Switch>
+          </div>
         </div>
-      </Fragment>
+      </Router>
     );
   }
 }
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
 import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types'
 
 const Users = ({ users, loading }) =>
-    loading ? Spinner : <div style={userStyle}>
+    loading ? <Spinner /> : <div style={userStyle}>
         {users.map(user => (
             <UserItem key={user.id} user={user} />
         ))}
